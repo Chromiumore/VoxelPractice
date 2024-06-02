2b90c51dbbb82e542751410cc2a938ccdc924c1b
Chromiumore <parts.dima06@gmail.com>
2024-06-02 18:58:42 +0300

    VAO

--- VoxelEngine/main.cpp
+++ VoxelEngine/main.cpp
@@ -13,12 +13,16 @@ int WIDTH = 1280;
 int HEIGHT = 720;
 const char* TITLE = "voxels";
 
+float vertices[] = {
+    0.0f, 0.0f, 0.0f,
+    1.0f, 0.0f, 0.0f,
+    0.0f, 1.0f, 0.0f
+};
+
 int main()
 {
     Window::init(WIDTH, HEIGHT, TITLE);
     Events::init();
-
-    glClearColor(0, 0, 0, 1);
 
     Shader* shader = load_shader("main.glslv", "main.glslf");
     if (shader == nullptr) {
@@ -26,6 +30,24 @@ int main()
         Window::terminate();
         return -1;
     }
+
+    // Vertex Array Object
+    GLuint VAO, VBO;
+    glGenVertexArrays(1, &VAO);
+    // Создание вершинного буфера
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    // Позиция
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)(0*sizeof(GLfloat)));
+
+    glEnableVertexAttribArray(0);
+    // Отвязывем объект на всякий случай
+    glBindVertexArray(0);
+
+    glClearColor(0, 0, 0, 1);
 
     while (!Window::isShouldClose()) {  
         Events::pullEvents();
@@ -36,6 +58,12 @@ int main()
 
 
         glClear(GL_COLOR_BUFFER_BIT);
+
+        shader->use();
+        // Отрисовка
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
 
         Window::swap();
     }
